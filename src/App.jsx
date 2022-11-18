cfad656a9d62e5240f7283793e15e675654f2d0c
Brandon Gustrowsky <brandon@may21.us>
2022-11-18 17:14:15 -0500

    Working on creating a decent color palette

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
 import { useState } from 'react'
+import { createTheme } from '@mui/material/styles';
 import Dashboard from "./components/Dashboard"
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+
+  let theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0B3954" //dark blue
+      },
+      secondary: {
+        main: "#BFD7EA" //light blue
+      },
+      orange: {
+        main: "#FF6663", //orange
+      },
+      neon: {
+        main: "#E0FF4F", //neon green
+      },
+      white: {
+        main: "#FEFFFE" //white
+      }
+    }
+  })
+
+ theme = createTheme(theme, {
+  components: {
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          background: theme.palette.orange.main,
+        }
+      }
+    },
+  }
+})
+
+console.log(theme.palette.secondary.main)
 
   return (
     <>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,6 +4,7 @@ const Title = () => {
     return (
         <>
             <Paper elevation={3} style={{width: "400px", height: "400px"}} >
+                
             </Paper>
         </>
         
