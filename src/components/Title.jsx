b1eeb3bd95f1925d77597faf840669829d305da5
Brandon Gustrowsky <brandon@may21.us>
2022-12-04 21:08:33 -0500

    Renaming title works correctly

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,16 @@
-import { Button, TextField } from '@mui/material';
-import { useState } from "react"
+import { Button, fabClasses, TextField } from '@mui/material';
+import { useEffect, useState } from "react"
 
 const Title = (props) => {
     const { text, url, plantName, setData } = props
 
     const [currPlantName, setCurrPlantName] = useState(plantName)
     const [isEditing, setIsEditing] = useState(false)
+    const [hasBeenUpdated, setHasBeenUpdated] = useState(false)
 
     const sendData = async (event) => {
         const { value } = event.target
         console.log(value)
-        setIsEditing(false)
         try {
             const response = await fetch(url.BASE_URL + "/plant-name" + url.apiKeyLink, {
                 method: "PUT",
@@ -22,26 +22,29 @@ const Title = (props) => {
                     name: `${value}`
                 })
             })
-            console.log(response)
+            if (response.status == 200) {
+                console.log(response)
+                setIsEditing(false)
+                setHasBeenUpdated(true)
+                console.log("Ran")
+            } else {
+                console.log("Plant name cannot be empty")
+            }
         } catch (error) {
             console.log(error)
         }
-    }
-
-    const updateTitle = (event) => {
-        setIsEditing(true)
     }
 
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
             <div className="titleContainer">
                 {isEditing ?
-                    <TextField autoFocus className="titleInput" variant="filled" value={ isEditing ? currPlantName : plantName}
+                    <TextField autoFocus className="titleInput" variant="filled" value={ (isEditing ? currPlantName : plantName) }
                         // onChange={(event) => { setData((prevData) => ({...prevData, "plant_name" : event.target.value})) }} onBlur={() => { setIsEditing(false) }}
                         onChange={(event) => { setCurrPlantName(event.target.value)}} onBlur={(event) => { sendData(event) }}
                         onKeyDown={(event) => { event.key === "Enter" ? sendData(event) : null }} />
                     :
-                    <Button variant="text" onDoubleClick={(event) => { updateTitle(event) }} className="titleReadOnly">{plantName}</Button>
+                    <Button variant="text" onDoubleClick={() => { setIsEditing(true) }} className="titleReadOnly">{ hasBeenUpdated ? currPlantName : plantName }</Button>
                 }
             </div>
         </div>
